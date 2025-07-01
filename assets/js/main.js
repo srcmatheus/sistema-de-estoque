d186eee7f1e2360ca87698859da62f216dbd4e79
srcmatheus <matheusgamarra009@gmail.com>
2025-07-01 19:39:30 -0400

    chore(js): estruturar pasta e arquivo inicial de módulos JavaScript

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*Arquivo principal, que inicializa todo o sistema organizando a ordem de execução*/
+
+/*Adicionando um evento ao documento. Quando a página terminar de carregar, as funções do sistema serão inicializadas*/
+document.addEventListener("DOMContentLoaded", () => {
+
+});
